faf5eefe25e5e4c5df4706fe5fefe7500bbca6dd
Molnár Zoltán <mozo37@gmail.com>
2024-05-18 17:56:40 +0200

    in main fixing 33. line

--- snake_game/main.py
+++ snake_game/main.py
@@ -1,5 +1,4 @@
 import time
-import turtle
 from turtle import Turtle, Screen
 
 from snake import Snake
@@ -31,7 +30,7 @@ while game_is_on:
     if snake.head.distance(food) < 15:
         food.refresh()
         snake.extend()
-        scoreboard.update_scoreboard()
+        scoreboard.increase_score()
 
     if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
         game_is_on = False
