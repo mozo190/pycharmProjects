cd0309e2c55c138ec9ed90628d13497a88f8417c
Molnár Zoltán <mozo37@gmail.com>
2024-05-19 19:17:44 +0200

    import pandas

--- day-25/main.py
+++ day-25/main.py
@@ -1,12 +1,16 @@
-import csv
+# import csv
+#
+# with open("./weather_data.csv") as data_file:
+#     # data = data_file.readlines()
+#     # print(data)
+#     data = csv.reader(data_file)
+#     temperatures = []
+#     for row in data:
+#         # print(row[1])
+#         if row[1] != "temp":
+#             temperatures.append(int(row[1]))
+#     print(temperatures)
+import pandas
 
-with open("./weather_data.csv") as data_file:
-    # data = data_file.readlines()
-    # print(data)
-    data = csv.reader(data_file)
-    temperatures = []
-    for row in data:
-        # print(row[1])
-        if row[1] != "temp":
-            temperatures.append(int(row[1]))
-    print(temperatures)
+data = pandas.read_csv("weather_data.csv")
+print(data["temp"])
