784a198f0efc34c206fdb3a0c58010d3b7f83cc5
Molnár Zoltán <mozo37@gmail.com>
2024-05-19 19:11:57 +0200

    temperaturas made

--- day-25/main.py
+++ day-25/main.py
@@ -1,3 +1,12 @@
+import csv
+
 with open("./weather_data.csv") as data_file:
-    data = data_file.readlines()
-    print(data)
+    # data = data_file.readlines()
+    # print(data)
+    data = csv.reader(data_file)
+    temperatures = []
+    for row in data:
+        # print(row[1])
+        if row[1] != "temp":
+            temperatures.append(int(row[1]))
+    print(temperatures)
