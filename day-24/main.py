0920591313f0a3e63c792f2521a51bedbde0a508
Molnár Zoltán <mozo37@gmail.com>
2024-05-19 17:36:52 +0200

    file reader/writer

--- day-24/main.py
+++ day-24/main.py
@@ -1,3 +1,6 @@
-file = open("my_file.txt")
-contents = file.read()
-print(contents)
+# with open("my_file.txt") as file:
+#     contents = file.read()
+#     print(contents)
+
+with open("new_file.txt", mode="w") as file:
+    file.write("\nnew text")
