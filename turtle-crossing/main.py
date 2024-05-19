6db44757f355f8ef8cf30f3e6c47b899b407a024
Molnár Zoltán <mozo37@gmail.com>
2024-05-19 09:47:25 +0200

    set up main with while loop

--- turtle-crossing/main.py
+++ turtle-crossing/main.py
@@ -1,3 +1,5 @@
+import time
+
 from scoreboard import Scoreboard
 from player import Player
 from turtle import Screen
@@ -6,10 +8,27 @@ screen = Screen()
 player = Player()
 scoreboard = Scoreboard()
 
+
+def stop_game():
+    global game_is_on
+    game_is_on = False
+
+
 screen.title("TURTLE CROSSING")
 screen.setup(width=600, height=600)
 screen.bgcolor("white")
+
 screen.tracer(0)
 screen.listen()
+screen.onkey(player.move_forward(), "Up")
+screen.onkey(player.move_backward(), "Down")
+
+screen.onkey(stop_game, "q")
+
+game_is_on = True
+
+while game_is_on:
+    time.sleep(0.1)
+    screen.update()
 
 screen.exitonclick()
