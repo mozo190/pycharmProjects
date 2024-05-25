0b765fa62e9ec93dbcafb425f4371dab6472a8e7
Molnár Zoltán <mozo37@gmail.com>
2024-05-25 12:27:29 +0200

    works

---
+++ day-27/main.py
@@ -0,0 +1,10 @@
+import tkinter
+
+window = tkinter.Tk()
+window.title("This is my first GUI program")
+window.minsize(width=500, height=300)
+
+my_label = tkinter.Label(text="I am a label", font=("Arial", 24, "bold"))
+my_label.pack()
+
+window.mainloop()
