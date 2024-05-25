9a384d5c483dede10dad43916c71faa8d7f5b0bc
Molnár Zoltán <mozo37@gmail.com>
2024-05-25 18:10:27 +0200

    finish with reset_timer function

--- pomodoroProject/main.py
+++ pomodoroProject/main.py
@@ -11,9 +11,17 @@ WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
 reps = 0
+timer = ""
 
 
 # ---------------------------- TIMER RESET ------------------------------- #
+def reset_timer():
+    window.after_cancel(timer)
+    canvas.itemconfig(timer_text, text="00:00")
+    timer_label.config(text="Timer")
+    tick_label.config(text="")
+    global reps
+    reps = 0
 
 # ---------------------------- TIMER MECHANISM ------------------------------- #
 def start_timer():
@@ -36,6 +44,7 @@ def start_timer():
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
 def count_down(count):
+
     count_min = math.floor(count / 60)
     count_sec = count % 60
     if count_sec == 0:
@@ -52,7 +61,8 @@ def count_down(count):
 
     canvas.itemconfig(timer_text, text=f"{count_min_zero}{count_min}:{count_sec_zero}{count_sec}")
     if count > 0:
-        window.after(1000, count_down, count - 1)
+        global timer
+        timer = window.after(1000, count_down, count - 1)
     else:
         start_timer()
         mark = ""
@@ -79,7 +89,7 @@ timer_label.grid(column=1, row=0)
 start_button = Button(text="Start", highlightthickness=0, command=start_timer)
 start_button.grid(column=0, row=2)
 
-reset_button = Button(text="Reset", highlightthickness=0)
+reset_button = Button(text="Reset", highlightthickness=0, command=reset_timer)
 reset_button.grid(column=2, row=2)
 
 tick_label = Label(font=(FONT_NAME, 24, "bold"), fg=GREEN, bg=YELLOW)
