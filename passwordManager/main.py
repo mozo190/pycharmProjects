cf3cd4cd59537ae412380eb2e66b6b3de01812b8
Molnár Zoltán <mozo37@gmail.com>
2024-05-25 18:54:11 +0200

    various labels created

--- passwordManager/main.py
+++ passwordManager/main.py
@@ -8,9 +8,24 @@ window = Tk()
 window.title("Password Manager")
 window.config(padx=20, pady=20)
 
-canvas = Canvas(width=250, height=300, highlightthickness=5, highlightcolor="black")
+canvas = Canvas(width=200, height=200, highlightthickness=5, highlightcolor="black")
 lock_img = PhotoImage(file="logo.png")
-canvas.create_image(125, 140, image=lock_img)
-canvas.pack()
+canvas.create_image(100, 100, image=lock_img)
+canvas.grid(column=1, row=0)
+
+website_label = Label(text="Website: ", font=("Arial", 10, "normal"))
+website_label.grid(column=0, row=1)
+
+website_input = Entry(width=35)
+website_input.grid(column=1, row=1, columnspan=2)
+
+email_label = Label(text="Email/Username: ", font=("Arial", 10, "normal"))
+email_label.grid(column=0, row=2)
+
+password_label = Label(text="Password: ", font=("Arial", 10, "normal"))
+password_label.grid(column=0, row=3)
+
+generate_password_label = Label(text="Generate Password", font=("Arial", 10, "normal"))
+generate_password_label.grid(column=2, row=3)
 
 window.mainloop()
