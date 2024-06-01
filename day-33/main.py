c470a5150981e45731d19bd2b56f1ddc608b0ecd
Molnár Zoltán <mozo37@gmail.com>
2024-06-01 19:27:42 +0200

    sunrise in Budapest now

--- day-33/main.py
+++ day-33/main.py
@@ -1,10 +1,20 @@
 import requests
 
-response = requests.get(url="http://api.open-notify.org/iss-now.json")
-print(response.status_code)
+# response = requests.get(url="http://api.open-notify.org/iss-now.json")
+# print(response.status_code)
+#
+# data = response.json()
+# long = float(data["iss_position"]["longitude"])
+# lat = float(data["iss_position"]["latitude"])
+# print(long)
+# print(lat)
 
+parameters = {
+    "lat": 47.497913,
+    "lng": 19.040236
+}
+
+response = requests.get("https://api.sunrise-sunset.org/json", params=parameters)
+response.raise_for_status()
 data = response.json()
-long = float(data["iss_position"]["longitude"])
-lat = float(data["iss_position"]["latitude"])
-print(long)
-print(lat)
+print(data)
