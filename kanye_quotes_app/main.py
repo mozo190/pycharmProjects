dfc1419ce4f01fb2c52b144b9974ff1f0609836e
Molnár Zoltán <mozo37@gmail.com>
2024-06-01 19:14:13 +0200

    get_quote method

--- kanye_quotes_app/main.py
+++ kanye_quotes_app/main.py
@@ -1,5 +1,18 @@
 from tkinter import *
-from tkinter import messagebox
+
+import requests
+
+
+def get_quote():
+    try:
+        response = requests.get(url="https://api.kanye.rest")
+        response.raise_for_status()
+        data = response.json()
+        quote = data["quote"]
+        canvas.itemconfig(quote_text, text=quote)
+    except requests.exceptions.RequestException as e:
+        canvas.itemconfig(quote_text, text="No internet connection.")
+
 
 window = Tk()
 window.title("Kenya quotes App")
@@ -12,7 +25,7 @@ quote_text = canvas.create_text(150, 207, text="Kenya quotes", width=250, font=(
 canvas.grid(row=0, column=0)
 
 kanye_img = PhotoImage(file="kanye.png")
-kanye_button = Button(image=kanye_img, highlightthickness=0)
+kanye_button = Button(image=kanye_img, highlightthickness=0, command=get_quote)
 kanye_button.grid(row=1, column=0)
 
 window.mainloop()
