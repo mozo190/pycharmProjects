3242701481a5f2a7c3d54a0c048707c3c655a99a
Molnár Zoltán <mozo37@gmail.com>
2024-06-01 18:49:04 +0200

    trying positions

--- day-33/main.py
+++ day-33/main.py
@@ -2,3 +2,9 @@ import requests
 
 response = requests.get(url="http://api.open-notify.org/iss-now.json")
 print(response.status_code)
+
+data = response.json()
+long = float(data["iss_position"]["longitude"])
+lat = float(data["iss_position"]["latitude"])
+print(type(long))
+print(type(lat))
