ca6d1a7a96e82f6106db562cee835036e70ddb8a
Molnár Zoltán <mozo37@gmail.com>
2024-06-01 09:53:42 +0200

    birthday

--- birthday-wisher-email-project/main.py
+++ birthday-wisher-email-project/main.py
@@ -5,5 +5,7 @@ year = now.year
 month = now.month
 day = now.day
 day_of_week = now.weekday()
+
+day_of_birthday = dt.datetime(year=1972, month=11, day=1)
 if year == 2024:
-    print("This is it.")
+    print(day_of_birthday)
