b41f8eaed451406e5ed8b7add4cab53634fbd764
Molnár Zoltán <mozo37@gmail.com>
2024-06-02 18:18:35 +0200

    next_card method

--- quizzler/main.py
+++ quizzler/main.py
@@ -12,7 +12,8 @@ for question in question_data:
 
 
 quiz = QuizBrain(question_bank)
-quiz_ui = UI()
+quiz_ui = UI(quiz)
+
 
 # while quiz.still_has_questions():
 #     quiz.next_question()
--- quizzler/ui.py
+++ quizzler/ui.py
@@ -1,20 +1,23 @@
 from tkinter import *
 
+from quiz_brain import QuizBrain
+
 THEME_COLOR = "#375362"
 
 
 class UI:
-    def __init__(self):
+    def __init__(self, quiz_brain: "QuizBrain"):
+        self.quiz = quiz_brain
         self.window = Tk()
         self.window.title("Flash Card")
         self.window.config(padx=20, pady=20, bg=THEME_COLOR)
 
-        self.canvas = Canvas(width=300, height=250, bg="white", highlightthickness=0)
-        self.card_title = self.canvas.create_text(150, 125, text="Title", font=("Ariel", 20, "italic"))
-        self.canvas.grid(row=1, column=0, columnspan=2, pady=20)
-
         self.score_label = Label(text="Score: 0", fg="white", bg=THEME_COLOR, font=("Ariel", 10, "bold"))
         self.score_label.grid(row=0, column=1, pady=20)
+
+        self.canvas = Canvas(width=300, height=250, bg="white", highlightthickness=0)
+        self.card_title = self.canvas.create_text(150, 125, text="Title", font=("Ariel", 20, "italic"), fill=THEME_COLOR)
+        self.canvas.grid(row=1, column=0, columnspan=2, pady=20)
 
         self.wrong_image = PhotoImage(file="images/false.png")
         self.wrong_button = Button(image=self.wrong_image, highlightthickness=0, command=self.next_card)
@@ -29,4 +32,5 @@ class UI:
         self.window.mainloop()
 
     def next_card(self):
-        pass
+        q_text = self.quiz.next_question()
+        self.canvas.itemconfig(self.card_title, text=q_text)
