25fc2d6a2c5db2d4c8fd131123377ee1d187e70a
Molnár Zoltán <mozo37@gmail.com>
2024-06-03 19:31:36 +0200

    key hidden

--- rain-alert/main.py
+++ rain-alert/main.py
@@ -1,22 +1,29 @@
 import requests
+from dotenv import load_dotenv
+import os
 
-# API_KEY: "cb39ed815882577596774c580048f8a3"
+load_dotenv()
 
+forecast_url = os.getenv('FORECAST_URL')
+api_key = os.getenv('API_KEY')
+
+# print(f"forecast_url: {forecast_url}")
+# print(f"api_key: {api_key}")
 parameters = {
     "lat": 44.34,
     "lon": 10.99,
-    "appid": "cb39ed815882577596774c580048f8a3",
+    "appid": api_key,
     "cnt": 4
 }
 
-response = requests.get("https://api.openweathermap.org/data/2.5/forecast?", params=parameters)
+response = requests.get(forecast_url, params=parameters)
 response.raise_for_status()
 weather_data = response.json()
-#print status code
+# print status code
 print(response.status_code)
 print(weather_data)
 
-#print "Bring an Umbrella" if any of the weather condition codes is less than 700 in the next 12 hours
+# print "Bring an Umbrella" if any of the weather condition codes is less than 700 in the next 12 hours
 weather_slice = weather_data["list"][:12]
 for hour_data in weather_slice:
     weather_code = hour_data["weather"][0]["id"]
