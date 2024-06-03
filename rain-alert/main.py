d0d1fb764c2dd3a4a145aa7600b6d506e92728b8
Molnár Zoltán <mozo37@gmail.com>
2024-06-03 18:48:40 +0200

    do we need umbrella?

--- rain-alert/main.py
+++ rain-alert/main.py
@@ -1,10 +1,12 @@
 import requests
+
 # API_KEY: "cb39ed815882577596774c580048f8a3"
 
 parameters = {
     "lat": 44.34,
     "lon": 10.99,
-    "appid": "cb39ed815882577596774c580048f8a3"
+    "appid": "cb39ed815882577596774c580048f8a3",
+    "cnt": 4
 }
 
 response = requests.get("https://api.openweathermap.org/data/2.5/forecast?", params=parameters)
@@ -14,3 +16,13 @@ weather_data = response.json()
 print(response.status_code)
 print(weather_data)
 
+#print "Bring an Umbrella" if any of the weather condition codes is less than 700 in the next 12 hours
+weather_slice = weather_data["list"][:12]
+for hour_data in weather_slice:
+    weather_code = hour_data["weather"][0]["id"]
+    if int(weather_code) < 700:
+        print("Bring an umbrella")
+        break
+    else:
+        print("No need for an umbrella")
+        break
