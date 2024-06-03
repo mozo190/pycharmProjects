06c66456497af2450b578c89426d141939660ee7
Molnár Zoltán <mozo37@gmail.com>
2024-06-03 18:06:42 +0200

    create a request

---
+++ rain-alert/main.py
@@ -0,0 +1,14 @@
+import requests
+# API_KEY: "cb39ed815882577596774c580048f8a3"
+
+parameters = {
+    "lat": 44.34,
+    "lon": 10.99,
+    "appid": "cb39ed815882577596774c580048f8a3"
+}
+
+response = requests.get("https://api.openweathermap.org/data/2.5/weather?", params=parameters)
+response.raise_for_status()
+weather_data = response.json()
+print(weather_data)
+
