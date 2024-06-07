9801c38dfb7d565b446635b9d5461219c113c0b9
Molnár Zoltán <mozo37@gmail.com>
2024-06-07 19:52:31 +0200

    pixel_creation_endpoint with pixel data

--- habit_tracker/main.py
+++ habit_tracker/main.py
@@ -24,3 +24,13 @@ graph_config = {
     "color": "ajisai"
 }
 response = requests.post(url=graph_endpoint, json=graph_config, headers={"X-USER-TOKEN": TOKEN})
+print(response.text)
+#https://pixe.la/v1/users/mozo190/graphs/graph1.html
+
+pixel_creation_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPH_ID}"
+pixel_data = {
+    "date": "20210701",
+    "quantity": "10.5"
+}
+
+response = requests.post(url=pixel_creation_endpoint, json=pixel_data, headers={"X-USER-TOKEN": TOKEN})
