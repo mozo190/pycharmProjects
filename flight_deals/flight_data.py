152820cf150c39e622176c2ecc52f7e60f4e7b47
Molnár Zoltán <mozo37@gmail.com>
2024-06-08 18:13:34 +0200

    find_cheapest_flight method created in flight_data.py

--- flight_deals/flight_data.py
+++ flight_deals/flight_data.py
@@ -1,14 +1,35 @@
 class FlightData:
-    def __init__(self, price, origin_city, origin_airport, destination_city, destination_airport, out_date,
+    def __init__(self, price, origin_airport, destination_airport, out_date,
                  return_date):
-        self.iata_code = ""
         self.price = price
-        self.origin_city = origin_city
         self.origin_airport = origin_airport
-        self.destination_city = destination_city
         self.destination_airport = destination_airport
         self.out_date = out_date
         self.return_date = return_date
 
-    def __str__(self):
-        return f"{self.destination_city}: {self.price}"
+    def find_cheapest_flight(flight_data):
+        if flight_data is None or not flight_data['data']:
+            print("No flights found.")
+            return FlightData("N/A", "N/A", "N/A", "N/A", "N/A")
+        first_flight = flight_data['data'][0]
+        lowest_price = float(first_flight['price']["grandTotal"])
+        origin = first_flight['itineraries'][0]['segments'][0]['departure']['iataCode']
+        destination = first_flight['itineraries'][0]['segments'][0]['arrival']['iataCode']
+        out_date = first_flight['itineraries'][0]['segments'][0]['departure']['at'].split("T")[0]
+        return_date = first_flight['itineraries'][1]['segments'][0]['arrival']['at'].split("T")[0]
+
+        cheapest_flight = FlightData(lowest_price, origin, destination, out_date, return_date)
+
+        for flight in flight_data['data']:
+            price = float(flight['price']["grandTotal"])
+            if price < lowest_price:
+                lowest_price = price
+                origin = flight['itineraries'][0]['segments'][0]['departure']['iataCode']
+                destination = flight['itineraries'][0]['segments'][0]['arrival']['iataCode']
+                out_date = flight['itineraries'][0]['segments'][0]['departure']['at'].split("T")[0]
+                return_date = flight['itineraries'][1]['segments'][0]['arrival']['at'].split("T")[0]
+                cheapest_flight = FlightData(price, origin, destination, out_date, return_date)
+                print(f"Lowest price to {destination} is HUF {lowest_price}")
+
+        return cheapest_flight
+
