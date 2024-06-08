2c847fa644b07740e51bd50d388622f20d1ff68f
Molnár Zoltán <mozo37@gmail.com>
2024-06-08 17:53:41 +0200

    in flight_search.py refresh get_destination_code

--- flight_deals/flight_search.py
+++ flight_deals/flight_search.py
@@ -2,27 +2,38 @@ import os
 
 import requests
 
+IATA_ENDPOINT = "https://test.api.amadeus.com/v1/reference-data/locations/cities"
+FLIGHT_ENDPOINT = "https://test.api.amadeus.com/v2/shopping/flight-offers"
+TOKEN_ENDPOINT = "https://test.api.amadeus.com/v1/security/oauth2/token"
+
 
 class FlightSearch:
     # This class is responsible for talking to the Flight Search API.
     def __init__(self):
-        self.endpoint = "https://tequila-api.kiwi.com"
         self.api_key = os.environ.get("FLIGHT_API_KEY")
-        self.headers = {
-            "apikey": self.api_key
-        }
-        self.params = {
-            "term": "London",
-            "location_types": "city",
-        }
+        self.api_secret = os.environ.get("FLIGHT_API_SECRET")
+        self._token = self._get_new_token()
 
     def get_destination_code(self, city_name):
-        self.params["term"] = city_name
-        response = requests.get(url=self.endpoint, headers=self.headers, params=self.params)
-        response.raise_for_status()
-        data = response.json()
-        # code = data["locations"][0]["code"]
-        code = "TESTING"
+        print(f"Using this token to get destination code: {self._token}")
+        headers = {"authorization": f"Bearer {self._token}"}  # "Bearer " + self._token
+
+        query = {
+            "keyword": city_name,
+            "max": "2",
+            "include": "AIRPORTS"
+        }
+
+        response = requests.get(url=IATA_ENDPOINT, headers=headers, params=query)
+        print(f"Status code: {response.status_code}. Airport IATA: {response.text}")
+        try:
+            code = response.json()["data"][0]["iataCode"]
+        except IndexError:
+            print(f"IndexError: No airport found for {city_name}")
+            return "N/A"
+        except KeyError:
+            print(f"KeyError: No airport found for {city_name}")
+            return "Not found"
         return code
 
     def search(self):
@@ -31,3 +42,17 @@ class FlightSearch:
         data = response.json()
         print(data)
         return data
+
+    def _get_new_token(self):
+        header = {
+            "Content-Type": "application/x-www-form-urlencoded"
+        }
+        body = {
+            "grant_type": "client_credentials",
+            "client_id": self.api_key,
+            "client_secret": self.api_secret
+        }
+        response = requests.post(url=TOKEN_ENDPOINT, headers=header, data=body)
+        print(f"Your token is: {response.json()['access_token']}")
+        print(f"Your token expires in: {response.json()['expires_in']} seconds")
+        return response.json()["access_token"]
