987d0bcfef2dc1b5110f52b58e68ab51b803071b
Molnár Zoltán <mozo37@gmail.com>
2024-06-08 17:26:11 +0200

    if sheet_data

--- flight_deals/main.py
+++ flight_deals/main.py
@@ -11,17 +11,15 @@ flight_search = FlightSearch()
 sheet_data = data_manager.get_data()
 print(sheet_data)
 
-# for row in data["munkalap1"]:
-#     if row["iataCode"] == "":
-#         flight_search.params["term"] = row["city"]
-#         flight_search.params["location_types"] = "city"
-#         flight_search.params["limit"] = 1
-#         flight_search.params["active_only"] = "true"
-#         flight_search.params["sort"] = "popularity"
-#         flight_search.params["locale"] = "en-US"
-#         flight_search.params["curr"] = "USD"
-#         flight_search.params["apikey"] = os.environ.get("FLIGHT_API_KEY")
-#         flight_data = flight_search.search()
-#         flight_data = FlightData(flight_data)
-#         row["iataCode"] = flight_data.iata_code
-#         print(flight_data.iata_code)
+if sheet_data[0]["iataCode"] == "":
+    for city in sheet_data:
+        city["iataCode"] = flight_search.get_destination_code(city["city"])
+        data_manager.destination_data = sheet_data
+        data_manager.update_data(sheet_data.index(city), city)
+        data_manager.save_data()
+        print(city)
+else:
+    print("IATA Codes already exist in the data.")
+    print(sheet_data)
+    print("No changes made.")
+
