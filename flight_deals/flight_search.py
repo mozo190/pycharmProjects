c540ae8ab485aaffb299fbb77da9309820f79d18
Molnár Zoltán <mozo37@gmail.com>
2024-06-08 16:09:32 +0200

    get_destination_code, main refresh

--- flight_deals/flight_search.py
+++ flight_deals/flight_search.py
@@ -1,8 +1,10 @@
 import os
 
+import requests
+
 
 class FlightSearch:
-    #This class is responsible for talking to the Flight Search API.
+    # This class is responsible for talking to the Flight Search API.
     def __init__(self):
         self.endpoint = "https://tequila-api.kiwi.com"
         self.api_key = os.environ.get("FLIGHT_API_KEY")
@@ -13,3 +15,19 @@ class FlightSearch:
             "term": "London",
             "location_types": "city",
         }
+
+    def get_destination_code(self, city_name):
+        self.params["term"] = city_name
+        response = requests.get(url=self.endpoint, headers=self.headers, params=self.params)
+        response.raise_for_status()
+        data = response.json()
+        # code = data["locations"][0]["code"]
+        code = "TESTING"
+        return code
+
+    def search(self):
+        response = requests.get(url=self.endpoint, headers=self.headers, params=self.params)
+        response.raise_for_status()
+        data = response.json()
+        print(data)
+        return data
--- flight_deals/main.py
+++ flight_deals/main.py
@@ -1,10 +1,34 @@
 import os
+import requests
 
 from data_manager import DataManager
 from flight_search import FlightSearch
 from flight_data import FlightData
 
+data_manager = DataManager()
+flight_search = FlightSearch()
+
+headers = {
+    "Authorization": os.environ.get("SHEET_AUTH_TOKEN")
+}
+
 flight_sheet_url = os.environ.get("SHEETY_ENDPOINT", "Message not found")
+sheety_response = requests.get(url=flight_sheet_url, headers=headers)
+sheety_response.raise_for_status()
+data = sheety_response.json()
+print(data)
 
-
-print(flight_sheet_url)
+for row in data["munkalap1"]:
+    if row["iataCode"] == "":
+        flight_search.params["term"] = row["city"]
+        flight_search.params["location_types"] = "city"
+        flight_search.params["limit"] = 1
+        flight_search.params["active_only"] = "true"
+        flight_search.params["sort"] = "popularity"
+        flight_search.params["locale"] = "en-US"
+        flight_search.params["curr"] = "USD"
+        flight_search.params["apikey"] = os.environ.get("FLIGHT_API_KEY")
+        flight_data = flight_search.search()
+        flight_data = FlightData(flight_data)
+        row["iataCode"] = flight_data.iata_code
+        print(flight_data.iata_code)
