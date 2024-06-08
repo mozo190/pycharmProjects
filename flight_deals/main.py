406d25e0e8946054738e16048313f413c5a07465
Molnár Zoltán <mozo37@gmail.com>
2024-06-08 17:58:53 +0200

    in main iterate sheet_data

--- flight_deals/main.py
+++ flight_deals/main.py
@@ -1,4 +1,6 @@
 import os
+import time
+
 import requests
 
 from data_manager import DataManager
@@ -11,15 +13,23 @@ flight_search = FlightSearch()
 sheet_data = data_manager.get_data()
 print(sheet_data)
 
-if sheet_data[0]["iataCode"] == "":
-    for city in sheet_data:
-        city["iataCode"] = flight_search.get_destination_code(city["city"])
-        data_manager.destination_data = sheet_data
-        data_manager.update_data(sheet_data.index(city), city)
-        data_manager.save_data()
-        print(city)
-else:
-    print("IATA Codes already exist in the data.")
-    print(sheet_data)
-    print("No changes made.")
+# if sheet_data[0]["iataCode"] == "":
+#     for city in sheet_data:
+#         city["iataCode"] = flight_search.get_destination_code(city["city"])
+#         data_manager.destination_data = sheet_data
+#         data_manager.update_data(sheet_data.index(city), city)
+#         data_manager.save_data()
+#         print(city)
+# else:
+#     print("IATA Codes already exist in the data.")
+#     print(sheet_data)
+#     print("No changes made.")
 
+for row in sheet_data:
+    if row["iataCode"] == "":
+        row["iataCode"] = flight_search.get_destination_code(row["city"])
+        time.sleep(2)
+print(f"sheet_data:\n {sheet_data}")
+
+data_manager.destination_data = sheet_data
+data_manager.update_destination_codes()
