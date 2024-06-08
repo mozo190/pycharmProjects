b077d8f9614442ca898a746931700cf4ee495931
Molnár Zoltán <mozo37@gmail.com>
2024-06-08 19:03:18 +0200

    endpoint fixed, data_manager.py reformatted

--- flight_deals/data_manager.py
+++ flight_deals/data_manager.py
@@ -47,30 +47,3 @@ class DataManager:
                 auth=self.authorization
             )
             print(sheety_response.text)
-    def add_data(self, new_data):
-        self.data.append(new_data)
-
-    def update_data(self, index, new_data):
-        self.data[index] = new_data
-
-    def delete_data(self, index):
-        self.data.pop(index)
-
-    def save_data(self):
-        with open("data.txt", mode="w") as file:
-            for item in self.data:
-                file.write(f"{item}\n")
-
-    def load_data(self):
-        with open("data.txt", mode="r") as file:
-            self.data = [item.strip() for item in file.readlines()]
-
-    def clear_data(self):
-        self.data = []
-        self.save_data()
-
-    def __str__(self):
-        return str(self.data)
-
-    def __repr__(self):
-        return str(self.data)
