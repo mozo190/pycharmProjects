b999fd7f984a8f566746c411071ec80c57049e4c
Molnár Zoltán <mozo37@gmail.com>
2024-06-15 12:17:37 +0200

    select_one method

--- beautifulSoup/main.py
+++ beautifulSoup/main.py
@@ -9,7 +9,10 @@ soup = BeautifulSoup(contents, "html.parser")
 soup.title.string = "Title changed using BeautifulSoup"
 # soup.findAll(name="a")[0].string = "Google"
 all_anchor_tags = soup.find_all(name="a")
-print(all_anchor_tags)
+# print(all_anchor_tags)
 
-for tag in all_anchor_tags:
-    print(tag.get("href"))
+# for tag in all_anchor_tags:
+    # print(tag.get("href"))
+
+company_url = soup.select_one(selector="#name")
+print(company_url)
