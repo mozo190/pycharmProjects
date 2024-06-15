467edfefdf896b7533c016c11bd95b4d67db3d86
Molnár Zoltán <mozo37@gmail.com>
2024-06-15 15:35:50 +0200

    writing the sorted list in text file

--- 100-movie-to-watch/main.py
+++ 100-movie-to-watch/main.py
@@ -17,12 +17,18 @@ find_all_title = soup.find_all(name="h3", class_="title")
 # print(pop)
 for title in find_all_title:
     text = title.getText().replace(")", "").replace(":", "")
-    int_text = int(text.split()[0])
+    int_text = num(text.split()[0])
     title_text = ' '.join(text.split()[1:])
     result.append((int_text, title_text))
 
 # print(result)
 sorted_result = sorted(result, key=lambda x: x[0])
 
-for i in sorted_result:
+# for i in sorted_result:
     # print(i[0], i[1])
+    # open("movies.txt", "a").write(f"{i[0]} {i[1]}\n")
+for num, text in sorted_result:
+    try:
+        open("movies.txt", "a").write(f"{num} {text}\n")
+    except FileNotFoundError:
+        open("movies.txt", "w").write(f"{num} {text}\n")
