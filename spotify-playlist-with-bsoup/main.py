7b78f02bf60abcdf9ba50795f2dc0b7315ea6e78
Molnár Zoltán <mozo37@gmail.com>
2024-06-15 17:23:08 +0200

    reformatted

--- spotify-playlist-with-bsoup/main.py
+++ spotify-playlist-with-bsoup/main.py
@@ -1,5 +1,5 @@
-from bs4 import BeautifulSoup
 import requests
+from bs4 import BeautifulSoup
 
 URL = "https://www.billboard.com/charts/hot-100/"
 time_travel = input("What year do you want to travel to? Type the date in this format YYYY-MM-DD: ")
@@ -12,8 +12,6 @@ song_name_spans = soup.select("li ul li h3")
 
 song_name_list = [song.getText().strip() for song in song_name_spans]
 
-
-    # print(song_name_list)
 with open("songs.txt", "a", encoding="utf-8") as file:
     for song_list in song_name_list:
         file.write(f"{song_list}\n")
