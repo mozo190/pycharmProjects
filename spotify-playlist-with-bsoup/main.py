c39ea93e448f0f968fdaa59a2082926d5c8f52e1
Molnár Zoltán <mozo37@gmail.com>
2024-06-15 16:59:28 +0200

    song_name_list printed

--- spotify-playlist-with-bsoup/main.py
+++ spotify-playlist-with-bsoup/main.py
@@ -5,8 +5,12 @@ URL = "https://www.billboard.com/charts/hot-100/"
 time_travel = input("What year do you want to travel to? Type the date in this format YYYY-MM-DD: ")
 response = requests.get(URL + time_travel)
 billboard_web_page = response.text
-print(response.text)
+# print(response.text)
 
 
 soup = BeautifulSoup(billboard_web_page, "html.parser")
-all_elements = soup.select("li ul li h3")
+song_name_spans = soup.select("li ul li h3")
+for song in song_name_spans:
+    song_name_list = song.getText().replace("\n", "").replace("\n", "").replace("\t", "")
+
+    print(song_name_list)
