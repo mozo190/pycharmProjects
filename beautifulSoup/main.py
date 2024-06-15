b09c2bde92a3e095baa5185f1f9f2e1e0b822896
Molnár Zoltán <mozo37@gmail.com>
2024-06-15 13:32:06 +0200

    find the highest score and print text, link, score

--- beautifulSoup/main.py
+++ beautifulSoup/main.py
@@ -26,6 +26,7 @@ yc_web_page = response.text
 soup = BeautifulSoup(yc_web_page, "html.parser")
 articles = soup.find_all("tr", class_="athing")
 # print(articles)
+article_data = []
 for article in articles:
     title_line = article.find("span", class_="titleline")
     if title_line:
@@ -34,15 +35,22 @@ for article in articles:
             article_text = article_tag.getText()
             article_link = article_tag.get("href")
             # score = article_tag.get("score")
-            print(article_text)
-            print(article_link)
+            # print(article_text)
+            # print(article_link)
             # print(score)
 
-    subtext = article.find_next_sibling("tr").find("td", class_="subtext")
-    if subtext:
-        score_tag = subtext.find("span", class_="score")
-        if score_tag:
-            score = score_tag.getText().split(" ")[0]
-            print(score)
-        else:
-            print("No score found")
+            subtext = article.find_next_sibling("tr").find("td", class_="subtext")
+            if subtext:
+                score_tag = subtext.find("span", class_="score")
+                if score_tag:
+                    score = score_tag.getText()
+                    score_value = int(score.split(" ")[0])
+                    article_data.append((article_text, article_link, score_value))
+
+if article_data:
+    max_article = max(article_data, key=lambda x: x[2])
+    print("Article text: ", max_article[0])
+    print("Article link: ", max_article[1])
+    print("Article score: ", max_article[2])
+else:
+    print("No articles found")
