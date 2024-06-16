fc6a287a7e1fca986a51f503ef671aef1211eeed
Molnár Zoltán <mozo37@gmail.com>
2024-06-16 10:15:41 +0200

    import spotify

--- spotify-playlist-with-bsoup/main.py
+++ spotify-playlist-with-bsoup/main.py
@@ -1,5 +1,24 @@
+import os
+
 import requests
+import spotify
 from bs4 import BeautifulSoup
+
+client_id = os.environ.get("SPOTIFY_CLIENT_ID")
+client_secret = os.environ.get("SPOTIFY_CLIENT_SECRET")
+
+sp = spotify.Spotify(
+    auth_manager=SpotifyOAuth(
+        scope="user-library-read",
+        redirect_uri="http://example.com",
+        client_id=client_id,
+        client_secret=client_secret,
+        show_dialog=True,
+        cache_path="token.txt",
+        username="your-spotify-username",
+    )
+)
+user_id = sp.current_user()["id"]
 
 URL = "https://www.billboard.com/charts/hot-100/"
 time_travel = input("What year do you want to travel to? Type the date in this format YYYY-MM-DD: ")
