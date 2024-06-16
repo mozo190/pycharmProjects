30169fe51e8776931f9393067256126e0f260ba6
Molnár Zoltán <mozo37@gmail.com>
2024-06-16 16:56:13 +0200

    use key.enter

--- scrape_with-selenium/interaction.py
+++ scrape_with-selenium/interaction.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_experimental_option("detach", True)
@@ -7,7 +8,11 @@ chrome_options.add_experimental_option("detach", True)
 driver = webdriver.Chrome(options=chrome_options)
 driver.get("https://en.wikipedia.org/wiki/Main_Page")
 
-articles = driver.find_element(By.XPATH, '//*[@id="articlecount"]/a[1]')
-print(articles.text)
+# articles = driver.find_element(By.XPATH, '//*[@id="articlecount"]/a[1]')
+# print(articles.text)
 
-driver.quit()
+search = driver.find_element(By.NAME, "search")
+search.send_keys("Python", Keys.ENTER)
+
+
+# driver.quit()
