f925d5acb8db4abeab1983bed30b1bcf91dcaa88
Molnár Zoltán <mozo37@gmail.com>
2024-06-16 17:24:25 +0200

    get items, prices and with for loop into dict

--- scrape_with-selenium/cookie-clicking.py
+++ scrape_with-selenium/cookie-clicking.py
@@ -12,3 +12,18 @@ cookie = driver.find_element(By.ID, "cookie")
 cookie.click()
 
 # Get the cookie count
+cookie_count = driver.find_element(By.ID, "money")
+cookie_count_text = cookie_count.text
+cookie_count_value = int(cookie_count_text.split(" ")[0])
+
+# Get the upgrade items
+items = driver.find_elements(By.CSS_SELECTOR, "#store div")
+item_ids = [item.get_attribute("id") for item in items]
+
+# Create a dictionary of the items and their prices
+item_prices = [int(item.text.split("-")[1].strip().replace(",", "")) for item in items]
+store_items = {}
+
+for item in range(len(item_prices)):
+    store_items[item_ids[item]] = item_prices[item]
+
