66f68c631e5f6df515dd38c29b08fdcbb099e72f
Molnár Zoltán <mozo37@gmail.com>
2024-06-16 13:06:09 +0200

    set up email sender

--- automated_prize_tracker/main.py
+++ automated_prize_tracker/main.py
@@ -1,5 +1,15 @@
-import smtplib
 import os
-my_email = "MY_EMAIL"
-password = "MY_PASSWORD"
+import smtplib
 
+my_email = os.environ.get("MY_EMAIL")
+password = os.environ.get("MY_PASSWORD")
+recipient_email = os.environ.get("RECIPIENT_EMAIL")
+
+
+with smtplib.SMTP("smtp.gmail.com", 587) as connection:
+    connection.starttls()
+    connection.login(user=my_email, password=password)
+    connection.sendmail(
+        from_addr=my_email,
+        to_addrs=recipient_email,
+        msg="Subject:Hello\n\nThis is the body of my email.")
