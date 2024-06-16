d9449474ffa8d2508455a6d2f144be9a8639ea8f
Molnár Zoltán <mozo37@gmail.com>
2024-06-16 17:03:55 +0200

    fil the f/lname, email and aut click

---
+++ scrape_with-selenium/sign_up_automaticly.py
@@ -0,0 +1,21 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_experimental_option("detach", True)
+
+driver = webdriver.Chrome(options=chrome_options)
+driver.get("http://secure-retreat-92358.herokuapp.com/")
+
+first_name = driver.find_element(By.NAME, "fName")
+first_name.send_keys("Zoltan")
+
+last_name = driver.find_element(By.NAME, "lName")
+last_name.send_keys("Molnar")
+
+email = driver.find_element(By.NAME, "email")
+email.send_keys("something@gmail.com")
+
+button = driver.find_element(By.TAG_NAME, "button")
+button.click()
