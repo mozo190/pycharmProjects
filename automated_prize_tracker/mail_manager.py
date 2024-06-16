31cdf562652cafa5d8cb1faa465561faa0711ade
Molnár Zoltán <mozo37@gmail.com>
2024-06-16 14:18:25 +0200

    separated mail manager

---
+++ automated_prize_tracker/mail_manager.py
@@ -0,0 +1,24 @@
+import os
+import smtplib
+
+my_email = os.environ.get("MY_EMAIL")
+password = os.environ.get("MY_PASSWORD")
+recipient_email = os.environ.get("RECIPIENT_EMAIL")
+sign = "Regards,\n\nMolnar Zoltan\nPhone: +36 309 776 039"
+
+subject = "Hello dear Zoltan\n\n"
+customer_name = "Dear Zoltan,"
+message = "This is the body message of the email."
+body = f"{customer_name}\n{message}\n\n{sign}"
+
+
+def send_email():
+    with smtplib.SMTP("smtp.gmail.com", 587) as connection:
+        connection.starttls()
+        connection.login(user=my_email, password=password)
+        connection.sendmail(
+            from_addr=my_email,
+            to_addrs=recipient_email,
+            msg=f"Subject:{subject} {body}"
+        )
+    print("Email sent successfully.")
--- automated_prize_tracker/main.py
+++ automated_prize_tracker/main.py
@@ -1,15 +1,3 @@
-import os
-import smtplib
+import mail_manager
 
-my_email = os.environ.get("MY_EMAIL")
-password = os.environ.get("MY_PASSWORD")
-recipient_email = os.environ.get("RECIPIENT_EMAIL")
-
-
-with smtplib.SMTP("smtp.gmail.com", 587) as connection:
-    connection.starttls()
-    connection.login(user=my_email, password=password)
-    connection.sendmail(
-        from_addr=my_email,
-        to_addrs=recipient_email,
-        msg="Subject:Hello\n\nThis is the body of my email.")
+mail_manager.send_email()
