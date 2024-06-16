40ab1b6d14cb1a788e1380e9ad48e87c7918bc51
Molnár Zoltán <mozo37@gmail.com>
2024-06-16 10:25:14 +0200

    song_uris created with try/except

--- spotify-playlist-with-bsoup/main.py
+++ spotify-playlist-with-bsoup/main.py
@@ -15,13 +15,25 @@ sp = spotify.Spotify(
         client_secret=client_secret,
         show_dialog=True,
         cache_path="token.txt",
-        username="your-spotify-username",
+        username="Mozo",
     )
 )
 user_id = sp.current_user()["id"]
 
 URL = "https://www.billboard.com/charts/hot-100/"
 time_travel = input("What year do you want to travel to? Type the date in this format YYYY-MM-DD: ")
+song_name_list = ["The list of song", "titles from your", "web scrape"]
+
+song_uris = []
+year = time_travel.split("-")[0]
+for song in song_name_list:
+    result = sp.search(q=f"track:{song} year:{year}", type="track")
+    try:
+        uri = result["tracks"]["items"][0]["uri"]
+        song_uris.append(uri)
+    except IndexError:
+        print(f"{song} doesn't exist in Spotify. Skipped.")
+
 response = requests.get(URL + time_travel)
 billboard_web_page = response.text
 # print(response.text)
