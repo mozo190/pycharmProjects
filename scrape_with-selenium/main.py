47fcde516acf4c9f4151ee1635582c29d599c638
Molnár Zoltán <mozo37@gmail.com>
2024-06-16 15:35:30 +0200

    getting path with Xpath

--- scrape_with-selenium/main.py
+++ scrape_with-selenium/main.py
@@ -7,12 +7,17 @@ chrome_options.add_experimental_option("detach", True)
 
 # Open the website
 driver = webdriver.Chrome(options=chrome_options)
-driver.get("https://www.amazon.com/dp/B075CYMYK6?ref_=cm_sw_r_cp_ud_ct_FM9M699VKHTT47YD50Q6&th=1")
+# driver.get("https://www.amazon.com/dp/B075CYMYK6?ref_=cm_sw_r_cp_ud_ct_FM9M699VKHTT47YD50Q6&th=1")
+#
+# price_dollar = driver.find_element(By.CLASS_NAME, "a-price-symbol")
+# price_amount = driver.find_element(By.CLASS_NAME, "a-price-whole")
+# price_cents = driver.find_element(By.CLASS_NAME, "a-price-fraction")
+# print(f"the price is {price_dollar.text}{price_amount.text}.{price_cents.text}")
 
-price_dollar = driver.find_element(By.CLASS_NAME, "a-price-symbol")
-price_amount = driver.find_element(By.CLASS_NAME, "a-price-whole")
-price_cents = driver.find_element(By.CLASS_NAME, "a-price-fraction")
-print(f"the price is {price_dollar.text}{price_amount.text}.{price_cents.text}")
+driver.get("https://www.python.org/")
+bug_link = driver.find_element(By.XPATH, '//*[@id="site-map"]/div[2]/div/ul/li[3]/a')
+print(bug_link.text)
+# bug_link.click()
 
 # driver.close()  # Close the one tab browser
 driver.quit()  # Close the whole browser
