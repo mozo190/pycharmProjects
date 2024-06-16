62639ef40f1ee767ac3f7e548c754145bebf3728
Molnár Zoltán <mozo37@gmail.com>
2024-06-16 19:30:02 +0200

    interesting development

---
+++ job-application-linkedin/main.py
@@ -0,0 +1,46 @@
+import os
+import time
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException
+
+link_email = os.environ.get("MY_EMAIL")
+link_password = os.environ.get("LINKEDIN_PASS")
+phone = os.environ.get("PHONE")
+url = ("https://www.linkedin.com/jobs/search/?currentJobId=3947629969&distance=25&f_AL=true&f_TPR="
+       "r604800&geoId=106079947&keywords=python%20developer&origin="
+       "JOB_SEARCH_PAGE_JOB_FILTER&refresh=true&spellCorrectionEnabled=true")
+
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_experimental_option("detach", True)
+
+driver = webdriver.Chrome(options=chrome_options)
+driver.get(url)
+button = driver.find_element(By.XPATH, '/html/body/div[3]/header/nav/div/a[2]')
+button.click()
+
+username = driver.find_element(By.ID, "username")
+username.send_keys(link_email)
+
+password = driver.find_element(By.ID, "password")
+password.send_keys(link_password)
+
+login_button = driver.find_element(By.XPATH, '//*[@id="organic-div"]/form/div[3]/button')
+login_button.click()
+
+time.sleep(10)
+
+jobs = driver.find_elements(By.CLASS_NAME, "job-card-container")
+for job in jobs:
+    job.click()
+    time.sleep(5)
+    try:
+        apply = driver.find_element(By.XPATH, '//*[@id="main-content"]/section/div[2]/section[1]/div[2]/div[2]/button')
+        apply.click()
+        time.sleep(5)
+        submit = driver.find_element(By.XPATH, '//*[@id="main-content"]/section/div[2]/section[1]/div[2]/div[2]/button')
+        submit.click()
+    except Exception as e:
+        print(e)
+        continue
