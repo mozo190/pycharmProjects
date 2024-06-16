44637c492a8026eb5d2a63bf4019ebc533923d0d
Molnár Zoltán <mozo37@gmail.com>
2024-06-16 17:20:05 +0200

    in cookie site aut click on cookie

--- scrape_with-selenium/sign_up_automaticly.py
+++ scrape_with-selenium/sign_up_automaticly.py
@@ -1,6 +1,5 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
 
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_experimental_option("detach", True)
