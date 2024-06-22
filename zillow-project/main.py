f29ba835ab1820ad8b5c28777ce5812c60b64740
Molnár Zoltán <mozo37@gmail.com>
2024-06-22 16:42:54 +0200

    another response no need, sleep(5)

--- zillow-project/main.py
+++ zillow-project/main.py
@@ -5,7 +5,6 @@ import requests
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
 
 sheet_research_link = os.environ.get('RESEARCH_LINK')
 zillow_link = os.environ.get('ZILLOW_ClONE')
@@ -43,12 +42,12 @@ paths = [link['href'] for link in props_link]
 # for link in paths:
 #     print(link)
 
-print(len(addresses), len(prices), len(paths))
-for i in range(len(addresses)):
-    print(f"Address: {addresses[i]}")
-    print(f"Price: {prices[i]}")
-    print(f"Link: {paths[i]}")
-    print("\n")
+# print(len(addresses), len(prices), len(paths))
+# for i in range(len(addresses)):
+#     print(f"Address: {addresses[i]}")
+#     print(f"Price: {prices[i]}")
+#     print(f"Link: {paths[i]}")
+#     print("\n")
 
 # open sheet form
 
@@ -60,18 +59,24 @@ driver.get(sheet_research_link)
 
 # fill in the form
 for i in range(len(addresses)):
-    address_input = driver.find_element(By.XPATH, '//*[@id="mG61Hd"]/div[2]/div/div[2]/div[1]/div/div/div[2]/div/div[1]/div/div[1]/input')
+    address_input = driver.find_element(By.XPATH,
+                                        '//*[@id="mG61Hd"]/div[2]/div/div[2]/div[1]/div/div/div[2]/div/div[1]/div/div[1]/input')
+    address_input.clear()
     address_input.send_keys(addresses[i])
 
-    price_input = driver.find_element(By.XPATH, '//*[@id="mG61Hd"]/div[2]/div/div[2]/div[2]/div/div/div[2]/div/div[1]/div/div[1]/input')
-
+    price_input = driver.find_element(By.XPATH,
+                                      '//*[@id="mG61Hd"]/div[2]/div/div[2]/div[2]/div/div/div[2]/div/div[1]/div/div[1]/input')
+    price_input.clear()
     price_input.send_keys(prices[i])
 
-    link_input = driver.find_element(By.XPATH, '//*[@id="mG61Hd"]/div[2]/div/div[2]/div[3]/div/div/div[2]/div/div[1]/div/div[1]/input')
+    link_input = driver.find_element(By.XPATH,
+                                     '//*[@id="mG61Hd"]/div[2]/div/div[2]/div[3]/div/div/div[2]/div/div[1]/div/div[1]/input')
+    link_input.clear()
     link_input.send_keys(paths[i])
 
     submit_button = driver.find_element(By.XPATH, '//*[@id="mG61Hd"]/div[2]/div/div[3]/div[1]/div[1]/div/span/span')
     submit_button.click()
 
-    another_response = driver.find_element(By.XPATH, '/html/body/div[1]/div[2]/div[1]/div/div[4]/a')
-    another_response.click()
+    # another_response = driver.find_element(By.XPATH, '/html/body/div[1]/div[2]/div[1]/div/div[4]/a')
+    # another_response.click()
+    driver.implicitly_wait(5)
