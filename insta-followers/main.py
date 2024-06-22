6e5073221bb8a170575330d325e81f78f427b2b1
Molnár Zoltán <mozo37@gmail.com>
2024-06-22 09:51:14 +0200

    environment, driver

--- insta-followers/main.py
+++ insta-followers/main.py
@@ -5,3 +5,10 @@ from selenium.common.exceptions import ElementClickInterceptedException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 
+insta = os.environ.get('INSTA')
+insta_password = os.environ.get('INSTA_PASSWORD')
+
+driver = webdriver.Chrome()
+driver.get('https://www.instagram.com/')
+
+sleep(2)
