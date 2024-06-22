2c37e02a53b263b8dca12116ff759071d85dcbe7
Molnár Zoltán <mozo37@gmail.com>
2024-06-22 10:10:27 +0200

    some try

--- insta-followers/main.py
+++ insta-followers/main.py
@@ -1,14 +1,18 @@
 import os
 from time import sleep
+
 from selenium import webdriver
-from selenium.common.exceptions import ElementClickInterceptedException
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
 
 insta = os.environ.get('INSTA')
-insta_password = os.environ.get('INSTA_PASSWORD')
+insta_password = os.environ.get('INSTA_PASS')
+insta_name = os.environ.get('INSTA_NAME')
 
 driver = webdriver.Chrome()
 driver.get('https://www.instagram.com/')
+
+sleep(2)
+cookies = driver.find_element(By.XPATH, '/html/body/div[4]/div[1]/div/div[2]/div/div/div/div/div[2]/div/button[1]')  # Accept cookies
+cookies.click()
 
 sleep(2)
