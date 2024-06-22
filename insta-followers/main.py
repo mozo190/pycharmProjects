7353bed58fd068f0a78de007a0dc1bf267b7b7db
Molnár Zoltán <mozo37@gmail.com>
2024-06-22 09:48:59 +0200

    main, imports

---
+++ insta-followers/main.py
@@ -0,0 +1,7 @@
+import os
+from time import sleep
+from selenium import webdriver
+from selenium.common.exceptions import ElementClickInterceptedException
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+
