ff5db00dc321cf7f98931eb5bb3214c2bb564728
Molnár Zoltán <mozo37@gmail.com>
2024-06-23 16:40:28 +0200

    inner outer functions

--- hello_flask/hello/hello.py
+++ hello_flask/hello/hello.py
@@ -6,3 +6,17 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
     return "Hello, World!"
+
+
+def outer_function():
+    print("I'm outer f")
+
+    def nested_function():
+        print("I'm inner f")
+
+    return nested_function
+
+
+inner_f = outer_function()
+
+inner_f()
