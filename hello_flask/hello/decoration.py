9c178920329f4a61e4c4bbdad4c488974a16c0f0
Molnár Zoltán <mozo37@gmail.com>
2024-06-24 19:58:08 +0200

    in decoration(func): wrapper()

--- hello_flask/hello/decoration.py
+++ hello_flask/hello/decoration.py
@@ -4,3 +4,10 @@ current_time = time.time()
 print(current_time)
 
 def speed_calc_decoration(func):
+    def wrapper():
+        start = time.time()
+        func()
+        end = time.time()
+        print(f"Time taken: {end - start}s")
+        return wrapper()
+
