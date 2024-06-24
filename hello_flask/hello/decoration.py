7a3b3dd1707e28ac1288d560ed23039862e14f45
Molnár Zoltán <mozo37@gmail.com>
2024-06-24 20:08:06 +0200

    reformatted and how it works?

--- hello_flask/hello/decoration.py
+++ hello_flask/hello/decoration.py
@@ -1,19 +1,24 @@
 import time
 
 current_time = time.time()
-print(current_time)
+
+
+# print(current_time)
 
 def speed_calc_decoration(func):
     def wrapper():
         start = time.time()
         func()
         end = time.time()
-        print(f"Time taken: {end - start}s")
+        print(f"{func.__name__} Time taken: {end - start}s")
+
     return wrapper()
+
 
 @speed_calc_decoration
 def fast_function():
     print("I'm a fast function")
+
 
 @speed_calc_decoration
 def slow_function():
