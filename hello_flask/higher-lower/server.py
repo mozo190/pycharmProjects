c78ea82eea644512c838481a7533d2e6e63322ed
Molnár Zoltán <mozo37@gmail.com>
2024-06-27 17:06:50 +0200

    completed the challenge

--- hello_flask/higher-lower/server.py
+++ hello_flask/higher-lower/server.py
@@ -1,3 +1,5 @@
+import random
+
 from flask import Flask
 
 app = Flask(__name__)
@@ -5,7 +7,20 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "<h1>Guess a number between 0 and 9</h1><img src='https://media1.giphy.com/media/v1.Y2lkPTc5MGI3NjExNnFraWRmdnI0NnF1Z3A0anIzNG9scnU0aXR1YXB6N2Fza2x2ejg5ciZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/LzwcNOrbA3aYvXK6r7/giphy.webp'>"
+    return "<h1>Guess a number between 0 and 9</h1><img src='https://media.giphy.com/media/3o7aCSPqXE5C6T8tBC/giphy.gif'>"
+
+
+rand_num = random.randint(0, 9)
+
+
+@app.route("/<int:guess>")
+def guess_number(guess):
+    if guess > rand_num:
+        return "<h1 style='color: red'>Too high, try again!</h1><img src='https://media.giphy.com/media/3o6ZtaO9BZHcOjmErm/giphy.gif' width=1200>"
+    elif guess < rand_num:
+        return "<h1 style='color: purple'>Too low, try again!</h1><img src='https://media.giphy.com/media/jD4DwBtqPXRXa/giphy.gif' width=1200>"
+    else:
+        return "<h1 style='color: green'>You found me!</h1>'<img src='https://media.giphy.com/media/4T7e4DmcrP9du/giphy.gif' width='1200'>"
 
 
 if __name__ == "__main__":
