0616d13f085968b7e112a437d7e492e9e885a35c
Molnár Zoltán <mozo37@gmail.com>
2024-06-29 17:05:27 +0200

    posts requests in object[]

--- hello_flask/my-blog-website/main.py
+++ hello_flask/my-blog-website/main.py
@@ -1,18 +1,22 @@
 import requests
 from flask import Flask, render_template
+from post import Post
+
+posts = requests.get("https://api.npoint.io/c790b4d5cab58020d391")
+post_objects = []
+for post in posts.json():
+    post_obj = Post(post["id"], post["title"], post["subtitle"], post["body"])
+    post_objects.append(post_obj)
 
 app = Flask(__name__)
 
 
 @app.route("/")
-def hello():
-    response = requests.get("https://api.npoint.io/c790b4d5cab58020d391")
-    data = response.json()
-
-    return render_template("index.html", posts=data)
+def get_all_posts():
+    return render_template("index.html", posts=post_objects)
 
 
-@app.route("/blog")
+@app.route("/post/<int:index>")
 def blog():
     return render_template("post.html")
 
