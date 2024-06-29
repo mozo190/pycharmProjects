b707a5b771ce613e5326aad7a55dc970a2d8ce18
Molnár Zoltán <mozo37@gmail.com>
2024-06-29 13:40:32 +0200

    datetime in copyright

--- hello_flask/use-jinja/server.py
+++ hello_flask/use-jinja/server.py
@@ -1,13 +1,15 @@
 from flask import Flask, render_template
 import random
+import datetime
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def hello():
+    current_year = datetime.datetime.now().year
     random_number = random.randint(0, 9)
-    return render_template("index.html", number=random_number)
+    return render_template("index.html", number=random_number, year=current_year)
 
 
 if __name__ == "__main__":
