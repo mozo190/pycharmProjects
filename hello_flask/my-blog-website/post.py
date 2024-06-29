f17c6d40713d94f00dbb27a8c464950332ede5a2
Molnár Zoltán <mozo37@gmail.com>
2024-06-29 17:04:40 +0200

    post.py fixed

--- hello_flask/my-blog-website/post.py
+++ hello_flask/my-blog-website/post.py
@@ -1,7 +1,6 @@
 class Post:
-    def __init__(self, title, content):
+    def __init__(self,post_id, title, subtitle, body):
+        self.id = post_id
         self.title = title
-        self.content = content
-
-    def __str__(self):
-        return f"{self.title}: {self.content}"
+        self.subtitle = subtitle
+        self.body = body
