1096929aae6ce498d5d5a7f529bf2bca1343674b
Molnár Zoltán <mozo37@gmail.com>
2024-07-01 19:04:14 +0200

    set up main and html-s

--- hello_flask/my-blog-website/main.py
+++ hello_flask/my-blog-website/main.py
@@ -5,7 +5,11 @@ from post import Post
 posts = requests.get("https://api.npoint.io/c790b4d5cab58020d391")
 post_objects = []
 for post in posts.json():
-    post_obj = Post(post["id"], post["title"], post["subtitle"], post["body"])
+    post_obj = Post(post["id"],
+                    post["title"],
+                    post["subtitle"],
+                    post["body"]
+                    )
     post_objects.append(post_obj)
 
 app = Flask(__name__)
--- hello_flask/my-blog-website/post.py
+++ hello_flask/my-blog-website/post.py
@@ -1,5 +1,5 @@
 class Post:
-    def __init__(self,post_id, title, subtitle, body):
+    def __init__(self, post_id, title, subtitle, body):
         self.id = post_id
         self.title = title
         self.subtitle = subtitle
