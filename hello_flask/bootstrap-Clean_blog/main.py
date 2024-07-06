122f1f28604bf6c17460ef06db0970e2b569dee8
Molnár Zoltán <mozo37@gmail.com>
2024-07-06 16:06:07 +0200

    contact() fixed with request.

--- hello_flask/bootstrap-Clean_blog/main.py
+++ hello_flask/bootstrap-Clean_blog/main.py
@@ -1,5 +1,5 @@
 import requests
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -26,8 +26,15 @@ def about():
     return render_template("about.html")
 
 
-@app.route('/contact')
+@app.route('/contact', methods=["GET", "POST"])
 def contact():
+    if request.method == "POST":
+        data = request.form
+        print(data["name"])
+        print(data["email"])
+        print(data["phone"])
+        print(data["message"])
+        return "h1>Successfully sent your message.</h1>"
     return render_template("contact.html")
 
 
