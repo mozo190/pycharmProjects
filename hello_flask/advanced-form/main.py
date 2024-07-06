5e9a3cae2c0b69a9bd1485fe0902e79bac096116
Molnár Zoltán <mozo37@gmail.com>
2024-07-06 17:31:50 +0200

    fill index.html, created /login

--- hello_flask/advanced-form/main.py
+++ hello_flask/advanced-form/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -6,6 +6,15 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return render_template("index.html")
+
+
+@app.route('/login', methods=["POST"])
+def login():
+    # return "💪 Success! Form submitted"
+    username = request.form["name"]
+    password = request.form["password"]
+    return f"💪 Success! Form submitted by {username} with password {password}"
+    # render_template("login.html", name=username, password=password))
 
 
 if __name__ == "__main__":
