207ca2594687e9ed4d2e498d862c593811abc130
Molnár Zoltán <mozo37@gmail.com>
2024-07-06 16:47:18 +0200

    mail_manager.py built

---
+++ hello_flask/bootstrap-Clean_blog/mail_manager.py
@@ -0,0 +1,18 @@
+import os
+import smtplib
+
+my_email = os.getenv("MY_EMAIL")
+password = os.getenv("MY_PASSWORD")
+recipient_email = os.getenv("RECIPIENT_EMAIL")
+
+
+def send_email(subject, body):
+    with smtplib.SMTP("smtp.gmail.com", 587) as connection:
+        connection.starttls()
+        connection.login(user=my_email, password=password)
+        connection.sendmail(
+            from_addr=my_email,
+            to_addrs=recipient_email,
+            msg=f"Suject:{subject}\n\n{body}"
+        )
+    print("Email sent successfully.")
