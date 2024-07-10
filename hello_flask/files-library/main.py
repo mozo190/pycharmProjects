ef37b08024d65eab64063a1bc9449172c09a65e0
Molnár Zoltán <mozo37@gmail.com>
2024-07-10 18:45:55 +0200

    Create a new table in the database

--- hello_flask/files-library/main.py
+++ hello_flask/files-library/main.py
@@ -2,14 +2,35 @@ from flask import Flask, render_template, request, redirect
 from flask_bootstrap import Bootstrap5
 # from flask_marshmallow import Marshmallow
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import Integer, String, Float
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 
 app = Flask(__name__)
 app.secret_key = "mysecret"
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///new-books-collection.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db = SQLAlchemy(app)
+db = SQLAlchemy(model_class=Base)
+db.init_app(app)
 # ma = Marshmallow(app)
 bootstrap = Bootstrap5(app)
+
+
+## Create a new table in the database
+class Book(db.Model):
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    title: Mapped[str] = mapped_column(String(250), nullable=False, unique=True)
+    author: Mapped[str] = mapped_column(String(250), nullable=False)
+    rating: Mapped[float] = mapped_column(Float, nullable=False)
+
+    # this will allow each book to be identified by its title when printed
+    def __repr__(self):
+        return f'<Book {self.title}>'
 
 all_books = []
 
