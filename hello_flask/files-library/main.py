4036ae96f53b777f2362d07d975c2d44c7468c87
Molnár Zoltán <mozo37@gmail.com>
2024-07-11 18:35:24 +0200

    in app.app_context method inserted try/exc

--- hello_flask/files-library/main.py
+++ hello_flask/files-library/main.py
@@ -1,8 +1,9 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, url_for
 from flask_bootstrap import Bootstrap5
 # from flask_marshmallow import Marshmallow
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import Integer, String, Float
+from sqlalchemy.exc import IntegrityError
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 
 app = Flask(__name__)
@@ -23,25 +24,29 @@ bootstrap = Bootstrap5(app)
 
 ## Create a new table in the database
 class Book(db.Model):
-    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, nullable=False, autoincrement=True)
     title: Mapped[str] = mapped_column(String(250), nullable=False, unique=True)
     author: Mapped[str] = mapped_column(String(250), nullable=False)
     rating: Mapped[float] = mapped_column(Float, nullable=False)
 
     # this will allow each book to be identified by its title when printed
-    def __repr__(self):
-        return f'<Book {self.title}>'
+    # def __repr__(self):
+    #     return f'<Book {self.title}>'
 
+    # create table schema in the database. Required application context
+    with app.app_context():
+        db.create_all()
 
-# create table schema in the database. Required application context
-with app.app_context():
-    db.create_all()
 
 # Create a new record in the database
 with app.app_context():
     new_book = Book(title="Harry Potter", author="J.K. Rowling", rating=9.3)
-    db.session.add(new_book)
-    db.session.commit()
+    try:
+        db.session.add(new_book)
+        db.session.commit()
+    except IntegrityError as e:
+        db.session.rollback()
+        print(f"Book already exists in the database: {e}")
 
 all_books = []
 
@@ -54,16 +59,17 @@ def home():
 @app.route('/add', methods=["GET", "POST"])
 def add_book():
     if request.method == "POST":
-        title = request.form.get("title")
-        author = request.form.get("author")
-        rating = request.form.get("rating")
+        title = request.form["title"]
+        author = request.form["author"]
+        rating = request.form["rating"]
         new_book_sample = {
             "title": title,
             "author": author,
             "rating": rating
         }
-        all_books.append(new_book_sample)
-        return redirect("/")
+        db.session.add(new_book_sample)
+        db.session.commit()
+        return redirect(url_for("home"))
     return render_template("add.html")
 
 
