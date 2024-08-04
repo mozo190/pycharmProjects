29ff60f01c64024f0d1cbe329b346743b6516d8d
Molnár Zoltán <mozo37@gmail.com>
2024-08-04 09:20:18 +0200

    in add_cafe method some change

--- hello_flask/cafe_api/main.py
+++ hello_flask/cafe_api/main.py
@@ -68,11 +68,11 @@ def add_cafe():
         name=request.form.get('name'),
         map_url=request.form.get('map_url'),
         img_url=request.form.get('img_url'),
-        location=request.form.get('location'),
-        has_sochets=bool(request.form.get('has_sockets')),
-        has_toilet=bool(request.form.get('has_toilet')),
-        has_wifi=bool(request.form.get('has_wifi')),
-        can_take_calls=bool(request.form.get('can_take_calls')),
+        location=request.form.get('loc'),
+        has_sochets=bool(request.form.get('sockets')),
+        has_toilet=bool(request.form.get('toilet')),
+        has_wifi=bool(request.form.get('wifi')),
+        can_take_calls=bool(request.form.get('calls')),
         seats=request.form.get('seats'),
         coffee_price=request.form.get('coffee_price')
     )
