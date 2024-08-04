f3f7a51001de16d358ef02f16cde81d91032ea18
Molnár Zoltán <mozo37@gmail.com>
2024-08-04 09:17:53 +0200

    add_cafe method implemented in route /add.

--- hello_flask/cafe_api/main.py
+++ hello_flask/cafe_api/main.py
@@ -62,6 +62,25 @@ def random():
 
 
 # HTTP POST - Create Record
+@app.route('/add', methods=['POST'])
+def add_cafe():
+    new_cafe = Cafe(
+        name=request.form.get('name'),
+        map_url=request.form.get('map_url'),
+        img_url=request.form.get('img_url'),
+        location=request.form.get('location'),
+        has_sochets=bool(request.form.get('has_sockets')),
+        has_toilet=bool(request.form.get('has_toilet')),
+        has_wifi=bool(request.form.get('has_wifi')),
+        can_take_calls=bool(request.form.get('can_take_calls')),
+        seats=request.form.get('seats'),
+        coffee_price=request.form.get('coffee_price')
+    )
+    db.session.add(new_cafe)
+    db.session.commit()
+    return jsonify(response={"success": "Successfully added the new cafe."})
+
+
 # HTTP PUT/PATCH - Update Record
 # HTTP DELETE - Delete Record
 
