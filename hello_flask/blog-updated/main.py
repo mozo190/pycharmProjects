2cdfbe3122b47839efd01550d6f20b389bb30148
Molnár Zoltán <mozo37@gmail.com>
2024-08-08 17:54:12 +0200

    in main.py row58,28 date fixed.

--- hello_flask/blog-updated/main.py
+++ hello_flask/blog-updated/main.py
@@ -25,7 +25,7 @@ class BlogPost(db.Model):
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
     title: Mapped[str] = mapped_column(String(250), unique=True, nullable=False)
     subtitle: Mapped[str] = mapped_column(String(250), nullable=False)
-    date_posted: Mapped[str] = mapped_column(String(250), nullable=False, default=datetime.now().strftime('%B %d, %Y'))
+    date: Mapped[str] = mapped_column(String(250), nullable=False, default=datetime.now().strftime('%B %d, %Y'))
     body: Mapped[str] = mapped_column(Text, nullable=False)
     author: Mapped[str] = mapped_column(String(250), nullable=False)
     img_url: Mapped[str] = mapped_column(String(250), nullable=False)
@@ -55,7 +55,7 @@ def add_new_post():
         new_post = BlogPost(
             title=request.form['title'],
             subtitle=request.form['subtitle'],
-            date_posted=datetime.now().strftime('%B %d, %Y'),
+            date=datetime.now().strftime('%B %d, %Y'),
             body=request.form['body'],
             author=request.form['author'],
             img_url=request.form['img_url']
