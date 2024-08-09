2c1cb7b012c097b6791b486f0e6c21c9a460d734
Molnár Zoltán <mozo37@gmail.com>
2024-08-09 19:21:26 +0200

    def show_post(post_id): in main changed

--- hello_flask/blog-updated/main.py
+++ hello_flask/blog-updated/main.py
@@ -58,7 +58,7 @@ def get_all_posts():
 # a route so that you can click in individual post
 @app.route('/post/<int:post_id>')
 def show_post(post_id):
-    requested_post = BlogPost.query.get_or_404(post_id)
+    requested_post = db.get_or_404(BlogPost, post_id)
     return render_template('post.html', post=requested_post)
 
 
