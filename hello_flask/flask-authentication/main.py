64dce79f62649a1dc7dd1404d0a43f4e1b4be42a
Molnár Zoltán <mozo37@gmail.com>
2024-08-11 09:41:32 +0200

    in main.py login() set up

--- hello_flask/flask-authentication/main.py
+++ hello_flask/flask-authentication/main.py
@@ -29,5 +29,10 @@ def register():
     return render_template('register.html')
 
 
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+
 if __name__ == '__main__':
     app.run(debug=True)
