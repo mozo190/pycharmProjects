f1b6f5c170d5e07ae5272552a35dca3a879d5f4e
Molnár Zoltán <mozo37@gmail.com>
2024-08-11 09:42:47 +0200

    in main.py logout() set up

--- hello_flask/flask-authentication/main.py
+++ hello_flask/flask-authentication/main.py
@@ -34,5 +34,10 @@ def login():
     return render_template('login.html')
 
 
+@app.route('logout')
+def logout():
+    return render_template('logout.html')
+
+
 if __name__ == '__main__':
     app.run(debug=True)
