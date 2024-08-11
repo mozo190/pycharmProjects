2c84fd7e145e364b72e851cde12bf3374e8755e4
Molnár Zoltán <mozo37@gmail.com>
2024-08-11 12:02:03 +0200

    reformatted

--- hello_flask/flask-authentication/main.py
+++ hello_flask/flask-authentication/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, send_file, send_from_directory
+from flask import Flask, render_template, request, send_from_directory
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import Integer, String
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
