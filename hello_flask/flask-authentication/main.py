bac8aa2334d6f29db64e3664f846af2070557bbc
Molnár Zoltán <mozo37@gmail.com>
2024-08-11 09:12:03 +0200

    sceleton set up

---
+++ hello_flask/flask-authentication/main.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'mysecretkey'
+
+
+# CREATE DATABASE
+class Base(DeclarativeBase):
+    pass
+
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+db = SQLAlchemy(model_class=Base)
+db.init_app(app)
+
+with app.app_context():
+    db.create_all()
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
