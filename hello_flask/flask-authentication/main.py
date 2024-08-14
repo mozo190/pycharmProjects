e898785d53e6e84baffe7a4fea72d0c698b3bd41
Molnár Zoltán <mozo37@gmail.com>
2024-08-14 19:39:07 +0200

    fixing table in database with the UserMixin

--- hello_flask/flask-authentication/main.py
+++ hello_flask/flask-authentication/main.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, send_from_directory
-from flask_login import LoginManager
+from flask_login import LoginManager, UserMixin
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import Integer, String
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
@@ -29,8 +29,8 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
 db = SQLAlchemy(model_class=Base)
 db.init_app(app)
 
-
-class User(db.Model):
+# fixing table in database with the UserMixin
+class User(UserMixin, db.Model):
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
     email: Mapped[str] = mapped_column(String(100), unique=True, nullable=False)
     password: Mapped[str] = mapped_column(String(100), nullable=False)
