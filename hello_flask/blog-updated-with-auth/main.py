a0a5b3d25228c2823da7741293f4fd40017f4379
Molnár Zoltán <mozo37@gmail.com>
2024-08-18 10:20:05 +0200

    Create a table in database

--- hello_flask/blog-updated-with-auth/main.py
+++ hello_flask/blog-updated-with-auth/main.py
@@ -1,16 +1,44 @@
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
 from flask_ckeditor import CKEditor
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import Integer, String, Text
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
 ckeditor = CKEditor(app)
 Bootstrap(app)
 
+# TODO: configure Flask-Login
+
+#Create a database
+class Base(DeclarativeBase):
+    pass
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db'
+db = SQLAlchemy(model_class=Base)
+db.init_app(app)
+
+#Create a table in database
+class BlogPost(db.Model):
+    __tablename__ = 'blog_posts'
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
+    title: Mapped[str] = mapped_column(String(250), unique=True, nullable=False)
+    subtitle: Mapped[str] = mapped_column(String(250), nullable=False)
+    date: Mapped[str] = mapped_column(String(250), nullable=False)
+    body: Mapped[str] = mapped_column(Text, nullable=False)
+    author: Mapped[str] = mapped_column(String(250), nullable=False)
+    img_url: Mapped[str] = mapped_column(String(250), nullable=False)
+
+with app.app_context():
+    db.create_all()
 
 @app.route('/')
-def home():
+def get_all_posts():
+    result = db.execute(db.select(BlogPost).order_by(Post.date.desc()))
     return render_template('index.html')
+
+@app.route
 
 
 if __name__ == "__main__":
