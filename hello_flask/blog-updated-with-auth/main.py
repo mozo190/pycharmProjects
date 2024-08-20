7876fa7c62b524c61127c0ad789d8900dde931ee
Molnár Zoltán <mozo37@gmail.com>
2024-08-20 20:57:34 +0200

    in main.py in edit_post() changed post_id=post.id)

--- hello_flask/blog-updated-with-auth/main.py
+++ hello_flask/blog-updated-with-auth/main.py
@@ -234,10 +234,10 @@ def edit_post(post_id):
         post.title = edit_form.title.data
         post.subtitle = edit_form.subtitle.data
         post.img_url = edit_form.img_url.data
-        post.author = current_user.name
+        post.author = current_user
         post.body = edit_form.body.data
         db.session.commit()
-        return redirect(url_for('show_post', post_id=post_id))
+        return redirect(url_for('show_post', post_id=post.id))
     return render_template('make-post.html', form=edit_form, is_edit=True)
 
 
