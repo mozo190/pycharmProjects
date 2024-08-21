df2ca9ab83c9f0433efe9305e9449c5272475211
Molnár Zoltán <mozo37@gmail.com>
2024-08-21 11:42:47 +0200

    in main.html add_new_post() removed author=current_user(.name),

--- hello_flask/blog-updated-with-auth/main.py
+++ hello_flask/blog-updated-with-auth/main.py
@@ -203,7 +203,7 @@ def add_new_post():
             subtitle=form.subtitle.data,
             body=form.body.data,
             img_url=form.img_url.data,
-            author=current_user.name,
+            author=current_user,
             date=date.today().strftime('%B %d, %Y')
         )
         db.session.add(new_post)
