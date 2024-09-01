3c2f0c7ba21e36f46dd7c2b7d7818b63cbb2d76a
Molnár Zoltán <mozo37@gmail.com>
2024-09-01 09:32:20 +0200

    main method expanded with 2 other options

--- 1_my_projects/morse_translator/main.py
+++ 1_my_projects/morse_translator/main.py
@@ -36,9 +36,22 @@ def convert_to_text(morse_code):
 
 
 def main():
-    text = input("Enter the text to convert to Morse code: ")
-    converted_text = convert_to_morse(text)
-    print(f'The Morse code for "{text}" is: {converted_text}')
+    prompt = input("Enter '1' to convert text to Morse code or "
+                   "'2' to convert Morse code to text or"
+                   " '3' if you want to quit: ")
+    if prompt == '1':
+        text = input("Enter the text to convert to Morse code: ")
+        converted_text = convert_to_morse(text)
+        print(f'The Morse code for "{text}" is: {converted_text}')
+    elif prompt == '2':
+        more_code = input("Enter the Morse code to convert to text: ")
+        converted_code = convert_to_text(more_code)
+        print(f'The text for "{more_code}" is: {converted_code}')
+    elif prompt == '3':
+        print("Goodbye!")
+        quit()
+    else:
+        print("Invalid input. Please try again.")
 
 
 if __name__ == "__main__":
