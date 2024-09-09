362903e272ec187c7bf73497999adf6949fc9183
Molnár Zoltán <mozo37@gmail.com>
2024-09-09 19:05:25 +0200

    jump(self)

--- pythonProject_game/Dino Game.py
+++ pythonProject_game/Dino Game.py
@@ -6,6 +6,8 @@ from kivy.uix.widget import Widget
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 250
 DINO_Y_POS = 30
+GRAVITY = -1.2
+JUMP_VELOCITY = 6
 
 Window.size = (SCREEN_WIDTH, SCREEN_HEIGHT)
 Window.clearcolor = (1, 1, 1, 1)
@@ -21,11 +23,22 @@ class Ground(Image):
 
 class Dino(Image):
     def __init__(self, **kwargs):
+        g = GRAVITY
+        up = JUMP_VELOCITY  # INITIAL VELOCITY
+        t = 0
+        jumping = False
+
         super(Dino, self).__init__(**kwargs)
         self.source = 'static/assets/img/sprites/dino_.png'
         self.width, self.height = (44, 48)
         self.size = (self.width, self.height)
         self.pos = (20, DINO_Y_POS)
+
+    def jump(self):
+        if not self.jumping:
+            self.jumping = True
+            self.up = JUMP_VELOCITY
+            self.t = 0
 
 
 class DinoGame(Widget):
@@ -37,6 +50,12 @@ class DinoGame(Widget):
 
         self.dino = Dino()  # Image(source='dino.png', pos=(100, 0))
         self.add_widget(self.dino)
+
+        Window.bind(on_key_down=self.on_key_down)
+
+    def on_key_down(self, window, key, *args):
+        if key == 32:  # space key
+            self.dino.jump()
 
 
 class DinoApp(App):
