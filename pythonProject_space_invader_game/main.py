579586139f2ff85abd36926697413e5aedc582a0
Molnár Zoltán <mozo37@gmail.com>
2024-09-13 19:30:38 +0200

    spaceship setup

--- pythonProject_space_invader_game/main.py
+++ pythonProject_space_invader_game/main.py
@@ -1,7 +1,9 @@
+
 from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.core.window import Window
 from kivy.graphics import Color, Rectangle
+from kivy.uix.image import Image
 
 #set the window size
 SCREEN_WIDTH = 800
@@ -16,6 +18,11 @@ class SpaceInvadersGame(Widget):
         with self.canvas:
             Color(0, 0, 0, 1)
             self.rect = Rectangle(size=(SCREEN_WIDTH, SCREEN_HEIGHT), pos=(0, 0))
+
+        # Add the spaceship image
+        self.spaceship = Image(source='static/assets/img/spaceship.png', size=(74, 74),
+                               pos=(SCREEN_WIDTH / 2 - 50, 20))
+        self.add_widget(self.spaceship)
 
 
 class SpaceInvadersApp(App):
