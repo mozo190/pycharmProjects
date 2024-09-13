c9ad30f9f611a06f585279dc2fbbb69fd82e69c9
Molnár Zoltán <mozo37@gmail.com>
2024-09-13 19:33:37 +0200

    spaceship setup details

--- pythonProject_space_invader_game/main.py
+++ pythonProject_space_invader_game/main.py
@@ -20,8 +20,12 @@ class SpaceInvadersGame(Widget):
             self.rect = Rectangle(size=(SCREEN_WIDTH, SCREEN_HEIGHT), pos=(0, 0))
 
         # Add the spaceship image
-        self.spaceship = Image(source='static/assets/img/spaceship.png', size=(74, 74),
-                               pos=(SCREEN_WIDTH / 2 - 50, 20))
+        # self.spaceship = Image(source='static/assets/img/spaceship.png', size=(74, 74),
+        #                        pos=(SCREEN_WIDTH / 2 - 50, 20))
+        self.spaceship = Image(source='static/assets/img/spaceship.png')
+        self.spaceship.size_hint = (None, None)
+        self.spaceship.size = (74, 74)
+        self.spaceship.pos = (SCREEN_WIDTH / 2 - self.spaceship.width / 2, 20)
         self.add_widget(self.spaceship)
 
 
