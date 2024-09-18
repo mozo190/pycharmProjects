4e76ec9a5e1d549fab71ec1ce4ac1163b857edc1
Molnár Zoltán <mozo37@gmail.com>
2024-09-18 18:53:03 +0200

    Add Ball widget to BrickBreakerGame
    Implemented a Ball class with position tracking and added it to the BrickBreakerGame. The Ball class uses Kivy's NumericProperty to manage its coordinates and update its position dynamically.

--- pythonProject-Brick_Breaker_game/main.py
+++ pythonProject-Brick_Breaker_game/main.py
@@ -1,21 +1,42 @@
-
 from kivy.app import App
-
+from kivy.properties import NumericProperty
 from kivy.uix.image import Image
 from kivy.uix.widget import Widget
 
+
+class Ball(Widget):
+    ball_x = NumericProperty(350)
+    ball_y = NumericProperty(300)
+
+    def __init__(self, **kwargs):
+        super(Ball, self).__init__(**kwargs)
+        self.WIDTH = 30
+        self.HEIGHT = 30
+        self.ballImage = Image(source='assets/img/ball.bmp')
+        self.ballImage.size = self.WIDTH, self.HEIGHT
+        self.add_widget(self.ballImage)
+
+        self.bind(ball_x=self.update_position, ball_y=self.update_position)
+        self.update_position()
+
+    def update_position(self, *args):
+        self.ballImage.pos = self.ball_x, self.ball_y
 
 class BrickBreakerGame(Widget):
     def __init__(self, **kwargs):
         super(BrickBreakerGame, self).__init__(**kwargs)
 
-        #add background image
+        # add background image
         self.background = Image(source='assets/img/bk.bmp')
         self.background.allow_stretch = True
         self.background.keep_ratio = False
         self.background.size = 800, 600
         self.background.pos = 0, 0
         self.add_widget(self.background)
+
+        # add ball
+        self.ball = Ball()
+        self.add_widget(self.ball)
 
 
 class BrickBreakerApp(App):
