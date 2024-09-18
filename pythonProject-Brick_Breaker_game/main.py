b58c3d67c1f5988ee42f0874210a15237d6d77d9
Molnár Zoltán <mozo37@gmail.com>
2024-09-18 20:59:19 +0200

    Add Bat class to main.py
    Introduced a new Bat class to represent the bat in the game. This class includes properties for bat position and an image widget. The bat's position is automatically updated using bindings.

--- pythonProject-Brick_Breaker_game/main.py
+++ pythonProject-Brick_Breaker_game/main.py
@@ -22,6 +22,26 @@ class Ball(Widget):
     def update_position(self, *args):
         self.ballImage.pos = self.ball_x, self.ball_y
 
+
+class Bat(Widget):
+    bat_x = NumericProperty(400)
+    bat_y = NumericProperty(30)
+
+    def __init__(self, **kwargs):
+        super(Bat, self).__init__(**kwargs)
+        self.WIDTH = 100
+        self.HEIGHT = 20
+        self.batImage = Image(source='assets/img/bat.bmp')
+        self.batImage.size = self.WIDTH, self.HEIGHT
+        self.add_widget(self.batImage)
+
+        self.bind(bat_x=self.update_position, bat_y=self.update_position)
+        self.update_position()
+
+    def update_position(self, *args):
+        self.batImage.pos = self.bat_x, self.bat_y
+
+
 class BrickBreakerGame(Widget):
     def __init__(self, **kwargs):
         super(BrickBreakerGame, self).__init__(**kwargs)
