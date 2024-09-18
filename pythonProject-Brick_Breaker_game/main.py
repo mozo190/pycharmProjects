2e00b710e99eb39b9467a6094534669d0e1f2cbc
Molnár Zoltán <mozo37@gmail.com>
2024-09-18 18:41:16 +0200

    Add background image to BrickBreakerGame
    Refactored BrickBreakerGame to a Widget class and included a background image. The image is set to stretch and fit the window size of 800x600. Removed the unused import of Window and its size setting.

--- pythonProject-Brick_Breaker_game/main.py
+++ pythonProject-Brick_Breaker_game/main.py
@@ -1,11 +1,21 @@
+
 from kivy.app import App
-from kivy.core.window import Window
 
-Window.size = (800, 600)
+from kivy.uix.image import Image
+from kivy.uix.widget import Widget
 
 
-class BrickBreakerGame:
-    pass
+class BrickBreakerGame(Widget):
+    def __init__(self, **kwargs):
+        super(BrickBreakerGame, self).__init__(**kwargs)
+
+        #add background image
+        self.background = Image(source='assets/img/bk.bmp')
+        self.background.allow_stretch = True
+        self.background.keep_ratio = False
+        self.background.size = 800, 600
+        self.background.pos = 0, 0
+        self.add_widget(self.background)
 
 
 class BrickBreakerApp(App):
