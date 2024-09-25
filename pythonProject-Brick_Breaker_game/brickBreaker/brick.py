c6d6ae90c74f530053c59b95d49398be8c64f3d6
Molnár Zoltán <mozo37@gmail.com>
2024-09-25 19:15:40 +0200

    Revise brick initialization method
    Updated `initialize_bricks` to dynamically set brick positions based on input rows and columns. Adjusted calculation of brick positions for better layout flexibility and set `no_of_bricks` accordingly.

--- pythonProject-Brick_Breaker_game/brickBreaker/brick.py
+++ pythonProject-Brick_Breaker_game/brickBreaker/brick.py
@@ -7,14 +7,18 @@ class Brick(Widget):
         super(Brick, self).__init__(**kwargs)
         self.brick_w = 50
         self.brick_h = 20
-        self.no_of_bricks = 21
+        self.no_of_bricks = 0
         self.brick_list = []
 
-    def initialize_bricks(self):
-        for row in range(450, 551, 50):
-            for col in range(50, 651, 100):
+    def initialize_bricks(self, rows, cols):
+        # add dynamically bricks
+        for row in range(rows):
+            for col in range(cols):
+                x_pos = 50 + col * (self.brick_w + 10)  # 50 is the starting x position
+                y_pos = 450 - row * (self.brick_h + 10)  # 450 is the starting y position
                 brick = Image(source='assets/img/brick.bmp')
                 brick.size = self.brick_w, self.brick_h
-                brick.pos = col, row
+                brick.pos = x_pos, y_pos
                 self.brick_list.append(brick)
                 self.add_widget(brick)
+                self.no_of_bricks = rows * cols  # calculate the number of bricks
