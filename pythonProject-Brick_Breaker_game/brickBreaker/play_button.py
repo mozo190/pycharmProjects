0d61a481a4e7ce9179953ec152f97d96fd3c0bd7
Molnár Zoltán <mozo37@gmail.com>
2024-09-27 18:21:04 +0200

    Add different button image for press action
    Replaced the play button image for the "down" action to improve visual feedback. Added a new image asset "button_press.png" for the pressed state.

--- pythonProject-Brick_Breaker_game/brickBreaker/play_button.py
+++ pythonProject-Brick_Breaker_game/brickBreaker/play_button.py
@@ -4,7 +4,8 @@ from kivy.uix.button import Button
 class PlayButton(Button):
     def __init__(self, **kwargs):
         super(PlayButton, self).__init__(**kwargs)
-        self.play_button = 'assets/img/play-button-icon.png'
+        self.background_normal = 'assets/img/play-button-icon.png'
+        self.background_down = 'assets/img/button_press.png'
         self.size_hint = None, None
         self.size = 200, 100
         self.pos_hint = {'center_x': 0.5, 'center_y': 0.5}
