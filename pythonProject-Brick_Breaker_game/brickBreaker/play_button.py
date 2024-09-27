5376bf144ad2cff9227f9366ca7d5d4b9b42150c
Molnár Zoltán <mozo37@gmail.com>
2024-09-27 18:44:38 +0200

    Add new play button icon and handle button press
    Updated the PlayButton class to use a new smaller icon and adjusted its position. Added properties to allow stretching and disabling aspect ratio, and implemented an on_press handler to print a message when the button is pressed.

--- pythonProject-Brick_Breaker_game/brickBreaker/play_button.py
+++ pythonProject-Brick_Breaker_game/brickBreaker/play_button.py
@@ -4,8 +4,15 @@ from kivy.uix.button import Button
 class PlayButton(Button):
     def __init__(self, **kwargs):
         super(PlayButton, self).__init__(**kwargs)
-        self.background_normal = 'assets/img/play-button-icon.png'
+        self.background_normal = 'assets/img/play-button-icon-small.png'
         self.background_down = 'assets/img/button_press.png'
         self.size_hint = None, None
         self.size = 200, 100
-        self.pos = 300, 200
+        self.pos = 300, 170
+        self.allow_stretch = True
+        self.keep_ratio = False
+
+        self.bind(on_press=self.on_press)
+
+    def on_press(self):
+        print("Play button pressed!")
