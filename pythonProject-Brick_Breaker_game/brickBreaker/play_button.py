7b7ca476a9c8126d85d67586e4d41d4c0a462fc8
Molnár Zoltán <mozo37@gmail.com>
2024-09-27 19:24:39 +0200

    Refactor PlayButton and remove instance parameter from restart_game
    Replaced Button with ButtonBehavior and Image for PlayButton to enhance its graphical interface. Removed the unnecessary instance parameter from restart_game to clean up the function signature.

--- pythonProject-Brick_Breaker_game/brickBreaker/play_button.py
+++ pythonProject-Brick_Breaker_game/brickBreaker/play_button.py
@@ -1,25 +1,20 @@
+from kivy.uix.behaviors import ButtonBehavior
 from kivy.uix.button import Button
+from kivy.uix.image import Image
 
 
-class PlayButton(Button):
+class PlayButton(ButtonBehavior, Image):
     def __init__(self, **kwargs):
         super(PlayButton, self).__init__(**kwargs)
-        self.background_normal = 'assets/img/play-button-icon-small.png'
-        self.background_down = 'assets/img/button_press.png'
+        self.source = 'assets/img/play-button-icon-small.png'
+        # self.background_down = 'assets/img/button_press.png'
         self.size_hint = None, None
-        self.size = 200, 100
-        self.pos = 300, 170
+        self.size = (200, 100)
+        self.pos = (300, 170)
         self.allow_stretch = True
-        self.keep_ratio = False
+        self.keep_ratio = True
 
         self.bind(on_press=self.on_press)
 
     def on_press(self):
-        print("Play button pressed!")
-
-    # override the on_touch_down method
-    def on_touch_down(self, touch):
-        if self.collide_point(*touch.pos):
-            super(PlayButton, self).on_touch_down(touch)
-            return True  # if the touch is on the button, return True
-        return False  # if the touch is not on the button, return False
+        self.parent.restart_game()
