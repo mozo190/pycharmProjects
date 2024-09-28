92044f1bd0bc8be150d15bd35424d95c1f681f49
Molnár Zoltán <mozo37@gmail.com>
2024-09-28 11:04:33 +0200

    Increase title font size in title_label.py
    Changed the font size of the game's title from 25sp to 50sp for better visibility and impact. This enhancement aims to make the title more prominent on the screen.

--- pythonProject-Helicopter_game/helicopterGame/title_label.py
+++ pythonProject-Helicopter_game/helicopterGame/title_label.py
@@ -8,9 +8,10 @@ class TitleLabel(Widget):
     def __init__(self, **kwargs):
         super(TitleLabel, self).__init__(**kwargs)
         self.title = Label(text='HELICOPTER GAME')
-        self.title.font_size = '25sp'
+        self.title.font_size = '50sp'
         self.title.pos = screenWidth / 2 - self.title.width / 2, screenHeight - 100
         self.add_widget(self.title)
+
         self.instructions = Label(text='Press SPACE to start')
         self.instructions.font_size = '25sp'
         self.instructions.pos = screenWidth / 2 - self.instructions.width / 2, screenHeight / 2
