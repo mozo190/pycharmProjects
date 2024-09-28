aad99b9f0b0bb278f2204be97624bb9d1cbe1be3
Molnár Zoltán <mozo37@gmail.com>
2024-09-28 13:41:15 +0200

    Add helicopter sound effects for movement
    Integrated sound effects for the helicopter movements using Kivy's SoundLoader. The sound plays when the helicopter moves up or down and stops when resetting the position. Added a new method to handle touch down events and trigger the corresponding sound effect.

--- pythonProject-Helicopter_game/helicopterGame/helicopter.py
+++ pythonProject-Helicopter_game/helicopterGame/helicopter.py
@@ -1,3 +1,4 @@
+from kivy.core.audio import SoundLoader
 from kivy.core.image import Image as CoreImage
 from kivy.graphics import Rectangle
 from kivy.uix.widget import Widget
@@ -16,6 +17,7 @@ class Helicopter(Widget):
         self.x = screenWidth / 2 - self.width / 2
         self.y = screenHeight / 2 - self.height / 2
         self.dy = 0  # initial vertical speed
+        self.helicopter_sound = SoundLoader.load('assets/audio/helicopter.wav')
 
         self.actor_image = CoreImage('assets/img/yellow-cartoon-helicopter.png').texture
         with self.canvas:
@@ -26,11 +28,17 @@ class Helicopter(Widget):
     def move_helicopter(self):
         self.y -= self.dy  # move the helicopter up or down
         self.rect.pos = (self.x, self.y)
+        self.helicopter_sound.play()
 
     def reset_position(self):
         self.x = screenWidth / 2 - self.width / 2
         self.y = screenHeight / 2 - self.height / 2
         self.dy = 0
+        self.helicopter_sound.stop()
 
         if hasattr(self, 'rect'):
             self.rect.pos = (self.x, self.y)
+
+    def on_touch_down(self, touch):  # move the helicopter up when the screen is touched
+        self.dy = upward_movement  # move the helicopter up
+        self.helicopter_sound.play()  # play the helicopter sound
