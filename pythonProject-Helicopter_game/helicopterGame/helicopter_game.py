b303558782eb63fa635ef569db891e4594bd2013
Molnár Zoltán <mozo37@gmail.com>
2024-09-28 10:54:05 +0200

    Add title and instructions to the game
    Introduced two labels for the game title and instructions using Kivy's Label widget. Set their positions dynamically based on screen dimensions from the configuration file.

--- pythonProject-Helicopter_game/helicopterGame/helicopter_game.py
+++ pythonProject-Helicopter_game/helicopterGame/helicopter_game.py
@@ -1,5 +1,7 @@
+from kivy.uix.label import Label
 from kivy.uix.widget import Widget
 
+from helicopterGame.config import screenWidth, screenHeight
 from helicopterGame.helicopter import Helicopter
 
 
@@ -9,3 +11,13 @@ class HelicopterGame(Widget):
 
         self.player = Helicopter()
         self.add_widget(self.player)
+
+        #add title and instructions to the game
+        self.title = Label(text='HELICOPTER GAME')
+        self.title.font_size = '25sp'
+        self.title.pos = screenWidth / 2 - self.title.width / 2 - 200, screenHeight - 100
+        self.add_widget(self.title)
+        self.instructions = Label(text='Press SPACE to start')
+        self.instructions.font_size = '25sp'
+        self.instructions.pos = screenWidth / 2 - 150, screenHeight / 2
+        self.add_widget(self.instructions)
