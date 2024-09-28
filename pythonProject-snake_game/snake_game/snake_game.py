c18454d6c58143a9587ffc67574f1dc9b87a3f3c
Molnár Zoltán <mozo37@gmail.com>
2024-09-28 14:56:53 +0200

    Set screen size configuration
    Added a new config file for screen size settings and updated the main game file to use these settings for window size. Also initialized background class for future enhancements.

--- pythonProject-snake_game/snake_game/snake_game.py
+++ pythonProject-snake_game/snake_game/snake_game.py
@@ -1,7 +1,11 @@
 from kivy.clock import Clock
+from kivy.core.window import Window
 from kivy.uix.widget import Widget
 
+from snake_game.config import SCREEN_HEIGHT, SCREEN_WIDTH
 from snake_game.snake import Snake
+
+Window.size = (SCREEN_WIDTH, SCREEN_HEIGHT)
 
 
 class SnakeGame(Widget):
