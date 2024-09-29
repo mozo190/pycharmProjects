b1cf3ca4d3f2492f9cc4c3f8f00452ef1efbad2b
Molnár Zoltán <mozo37@gmail.com>
2024-09-29 14:37:14 +0200

    Enable loop for background sound
    Set the background sound to loop continuously. This ensures that the soundtrack plays repeatedly without interruption during the game.

--- pythonProject-snake_game/snake_game/background_sound.py
+++ pythonProject-snake_game/snake_game/background_sound.py
@@ -4,6 +4,7 @@ from kivy.core.audio import SoundLoader
 class BackgroundSound:
     def __init__(self):
         self.sound = SoundLoader.load('assets/sound/piano-loops-background.wav')
+        self.sound.loop = True
 
     def play_background(self):
         self.sound.play()
